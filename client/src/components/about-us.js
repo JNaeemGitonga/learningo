95f4b7c623c126f4a116c3081ec9d80e03737419
PROB8 <blackb8r@gmail.com>
2017-09-27 10:51:33 -0400

    remove onClick for 'about us'

--- client/src/components/about-us.js
+++ client/src/components/about-us.js
@@ -5,19 +5,13 @@ import Pronunciations from './pronunciations';
 import {connect} from 'react-redux';
 
 export class AboutUs extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            display:'none',
-            toggleDisplay: () => this.state.display === 'none' ? this.setState({display:'block'}) :  this.setState({display:'none'})
-        }
-    }
+    
 
     render() {
         return (
             <div className='about-us'>
-                <h2 className='about-heading'><a href='javascript:void(0)' onClick={() => this.state.toggleDisplay()}>About Us...</a></h2>
-                <div style={{display:this.state.display}} className='about-container'>
+                <h2 className='about-heading'>About Us...</h2>
+                <div className='about-container'>
                     <p className='aboutUs'>Practice, practice, practice! Inside you'll find 
                         subjects to practice and if you don't find one you need, soon
                         you will be able to create your own lesson! Login with Google, and
